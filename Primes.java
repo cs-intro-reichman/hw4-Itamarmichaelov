4d66fce15a576c98ec3304e64271a07b8bfb35a1
unknown <itamar12332@gmail.com>
2024-12-06 00:42:47 +0200

    complted10

--- Primes.java
+++ Primes.java
@@ -1,34 +1,31 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
         int members = Integer.parseInt(args[0]);
-        boolean[] arr1 = new boolean[members+1];
+        boolean[] arr1 = new boolean[members + 1];
         int counter = 0;
-        for( int a = 2;  a <= members;a++){
+
+        // Initialize all numbers as potentially prime
+        for (int a = 2; a <= members; a++) {
             arr1[a] = true;
-        } 
-        for( int i = 2 ; i <= members;i ++ ){
-            for(int j = i+1 ;j <= members ;j++){
-                if( j % i == 0){
-                  arr1[j]= false;  
-                }    
-            }
-            if (arr1[i] == true ) {
-                System.out.println(i); 
-                counter++;  
+        }
+
+        // Apply the Sieve of Eratosthenes logic
+        for (int i = 2; i <= members; i++) {
+            for (int j = i * 2; j <= members; j += i) {
+                arr1[j] = false; // Mark multiples of i as non-prime
             }
         }
-       
 
-for (int i = 2; i <= members; i++) {
-    if (arr1[i]) {
-        System.out.println(i);
-        counter++;
+        // Print prime numbers and count them
+        System.out.println("Prime numbers up to " + members + ":");
+        for (int i = 2; i <= members; i++) {
+            if (arr1[i]) {
+                System.out.println(i);
+                counter++;
+            }
+        }
+
+        // Print the final count of primes
+        System.out.println("There are " + counter + " primes between 2 and " + members + " (" + counter + "% are primes)");
     }
-}
-
-
-double percentage = (counter / (double) members) * 100;
-System.out.println("There are " + counter + " primes between 2 to " + members + " (" + (int) percentage + "% are primes)");
-}
 }
